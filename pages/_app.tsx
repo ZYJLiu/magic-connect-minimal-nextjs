bfb871e78ebd816474f0949a79e20cacb3d259af
John <johnzhengyuliu@gmail.com>
2023-05-05 23:40:45 -0500

    setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import MagicProvider from "@/context/magic-context"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MagicProvider>
+      <Component {...pageProps} />
+    </MagicProvider>
+  )
 }
